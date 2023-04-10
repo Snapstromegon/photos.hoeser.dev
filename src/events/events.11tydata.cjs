36de5513adc06d276d6a1477f325106372a0ad4b
Raphael Höser <raphael@hoeser.info>
2023-04-10 13:14:02 +0200

    Clean up code
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- src/events/events.11tydata.cjs
+++ src/events/events.11tydata.cjs
@@ -8,20 +8,11 @@ module.exports = async (configData) => {
           shareUrl: album.url,
           ...data.googlePhotos?.allAlbums.find((a) => a.title == album.name),
         })),
-      coverUrl: (data) => {
-        const album = data.albums?.[0];
-        console.log("asd", album, album?.coverPhotoBaseUrl);
-        return album?.coverPhotoBaseUrl;
-      },
-      photosCount: (data) => {
-        console.log("hi", data.albums, data.albums?.[0]);
-        console.log(
-          data.albums ? data.albums?.map((album) => album.mediaItemsCount) : 0
-        );
-        return data.albums
+      coverUrl: (data) => data.albums?.[0]?.coverPhotoBaseUrl,
+      photosCount: (data) =>
+        data.albums
           ? data.albums?.reduce((acc, album) => acc + album.mediaItemsCount, 0)
-          : 0;
-      },
+          : 0,
     },
   };
 };
